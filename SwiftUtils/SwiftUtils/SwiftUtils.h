3cd38edb36e6447bccd90425815e36aea8fd9b6f
Alvaro Grimal Cabello <alvaro.grimal@babel.es>
2020-09-25 12:29:25 +0200

    C00248P001S00-14 Create project
    Why:
    - It is necessary to create the structure of project.
    
    How:
    - Created project and added functions.

---
+++ SwiftUtils/SwiftUtils/SwiftUtils.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUtils.h
+//  SwiftUtils
+//
+//  Created by alvaro.grimal.local on 25/09/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUtils.
+FOUNDATION_EXPORT double SwiftUtilsVersionNumber;
+
+//! Project version string for SwiftUtils.
+FOUNDATION_EXPORT const unsigned char SwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtils/PublicHeader.h>
+
+
